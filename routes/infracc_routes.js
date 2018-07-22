fa50e657495dd7274553c1d09513cbae07330863
DESKTOP-SNHP3V0\Usuario <jr.rodmed@gmail.com>
2018-07-22 12:01:03 +0200

    Created the peticions to mongo, by all and someone

--- routes/infracc_routes.js
+++ routes/infracc_routes.js
@@ -1,6 +1,9 @@
 module.exports = function(app, db) {
+    // Petición de una sola matrícula
     app.get('/infraccion/:Matricula', (req, res) => {
-        const details = { 'Matricula': '5649JSN' };
+        var matricula = req.params.Matricula;
+        console.log("Matricula pedida: " + matricula);
+        const details = { 'Matricula': matricula };
         db.collection('InfraccionesDet').findOne(details, (err, item) => {
             if (err) {
                 res.send({ 'error': 'error en el envio' });
@@ -10,8 +13,20 @@ module.exports = function(app, db) {
         })
     });
 
-    app.post('/infraccion', (req, res) => {
+    // Petición de todas las matrículas
+    app.get('/infraccion', (req, res) => {
+        console.log("Se piden todas las matriculas");
+        db.collection('InfraccionesDet').find({}).toArray(function(err, item) {
+            if (err) {
+                res.send({ 'error': 'error en el envio' });
+            } else {
+                res.send(item);
+            }
+        })
+    });
+
+    /*app.post('/infraccion', (req, res) => {
         console.log(req.body)
         res.send('hello')
-    });
+    });*/
 };
--- server.js
+++ server.js
@@ -12,6 +12,6 @@ MongoClient.connect(db.url, (err, database) => {
     if (err) return console.log(err);
     require('./routes')(app, database);
     app.listen(port, () => {
-        console.log('Estamos en vivo en ' + port);
+        console.log('Ejecutando backend en puerto ' + port);
     });
 })
